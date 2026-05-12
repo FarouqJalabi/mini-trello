0e69caa38ccc1340a6b47a247279c6b2897fd5a7
FarouqJalabi <farouqjalabi@gmail.com>
2026-05-12 15:08:48 +0200

    glitchtip works!

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,4 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   root "boards#index"
-  get "/debug-glitchtip" => proc { raise "Test GlitchTip error!" }
 end
