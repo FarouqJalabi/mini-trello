50b41f7dcaeaa619d26dd5ebf616cc64f606d2ac
FarouqJalabi <farouqjalabi@gmail.com>
2026-05-12 11:56:21 +0200

    confirmable

---
+++ db/migrate/20260512100204_add_missing_pending_confirmation.rb
@@ -0,0 +1,5 @@
+class AddMissingPendingConfirmation < ActiveRecord::Migration[8.1]
+  def change
+    add_column :users, :unconfirmed_email, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_12_074758) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_12_100204) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -85,13 +85,18 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_12_074758) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "confirmation_sent_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.string "unconfirmed_email"
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
